1596273957851f169f23f2e26602099ae756c63a
Morrow Davidson <morrow.davidson@gmail.com>
2019-05-16 16:37:17 -0700

    First Commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,12 +9,10 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <p>This is the future home of Taru Healing.</p>
+    <div style={{ maxWidth: `2000px`, marginBottom: `1.45rem`, height: '400px', overflow: 'hidden' }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
